92af478da34327371b1d25d6d13963628f4729e2
Ashish Dubey <ashish.dubey91@gmail.com>
2013-11-21 19:35:41 +0530

    bring them reducers on browser

--- example/map.js
+++ example/map.js
@@ -12,3 +12,11 @@ window.webmr.map = function(names) {
     });
     return results;
 };
+
+window.webmr.reduce = function(key, results) {
+    var sum = 0;
+    for (var i = 0; i < results.length; i++) {
+        sum += Number(results[i]);
+    }
+    return {key: key, val: sum};
+};
--- example/test.js
+++ example/test.js
@@ -4,8 +4,9 @@ var store = infantry.store.fs(__dirname + '/data');
 
 var env = {
 	store: store,
-	mapJS: __dirname + '/map.js',
-	reduce_func: require('./reduce.js').reduce_func
+	programSource: __dirname + '/map.js',
+	reduce_func: require('./reduce.js').reduce_func,
+	port: 8888
 };
 
 infantry.server(env);
