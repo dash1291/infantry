551af086a27c1d5e8f6fd615e07c3e89ef155dd7
Ashish Dubey <ashish.dubey91@gmail.com>
2013-12-03 12:59:25 +0530

    clean unused files

--- example/test.js
+++ example/test.js
@@ -5,7 +5,6 @@ var store = infantry.store.fs(__dirname + '/data');
 var env = {
 	store: store,
 	programSource: __dirname + '/map.js',
-	reduce_func: require('./reduce.js').reduce_func,
 	port: 8888
 };
 
